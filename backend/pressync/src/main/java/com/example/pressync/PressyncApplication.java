78caea8b9541cd79f59343901dc10de916b248f9
thorn <moraru.vlad34@gmail.com>
2025-11-29 22:13:38 +0200

    Added the backend struct

---
+++ backend/pressync/src/main/java/com/example/pressync/PressyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.pressync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PressyncApplication.class, args);
+	}
+
+}
---
+++ backend/pressync/src/test/java/com/example/pressync/PressyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pressync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
